b99ea030980afc4c84c05027c67b076ebe68f54e
Yukihiro Saito <yukky.saito@gmail.com>
2019-11-24 21:30:16 +0900

    update setup scripts

--- setup_ubuntu16.04.sh
+++ setup_ubuntu16.04.sh
@@ -6,7 +6,7 @@ then
   SCRIPT_DIR=$(cd $(dirname $0); pwd)
   cd $SCRIPT_DIR/ansible
   sudo apt install ansible
-	ansible-playbook -i localhost, $SCRIPT_DIR/ansible/localhost-setup-devpc.yml
+	ansible-playbook -i localhost, $SCRIPT_DIR/ansible/localhost-setup-ubuntu16.04-devpc.yml
 	echo -e "\e[32mok: Complete \e[0m"
 else
     echo -e "\e[33merror: Cannot Complete \e[0m"
