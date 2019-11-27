8bd932b62db1fa96b66d198b7204dfd44d750623
Kosuke Murakami <kosuke.murakmi@tier4.jp>
2019-11-27 12:06:48 +0900

    update
    Signed-off-by: Kosuke Murakami <kosuke.murakmi@tier4.jp>

--- src/planning/motion_planning/motion_base_planner/include/eb_path_planner/node.hpp
+++ src/planning/motion_planning/motion_base_planner/include/eb_path_planner/node.hpp
@@ -1,47 +1,23 @@
-// #pragma once
-// #include <autoware_planning_msgs/Path.h>
-// #include <autoware_planning_msgs/Trajectory.h>
-// #include <ros/ros.h>
-// #include <memory>
-// #include <tf2_ros/transform_listener.h>
-
-// namespace motion_planner
-// {
-// class BasePlannerNode
-// {
-// protected:
-//   ros::NodeHandle nh_, pnh_;
-//   ros::Publisher path_pub_;
-//   ros::Subscriber path_sub_;
-//   tf2_ros::Buffer tf_buffer_;
-//   tf2_ros::TransformListener tf_listener_;
-//   virtual bool callback(const autoware_planning_msgs::Path &input_path_msg, autoware_planning_msgs::Path &output_path_msg) = 0;
-//   void pathCallback(const autoware_planning_msgs::Path &input_path_msg);
-//   bool getSelfPose(geometry_msgs::TransformStamped& self_pose, const std_msgs::Header &header);
-//   bool getCurrentSelfPose(geometry_msgs::TransformStamped& self_pose);
-
-// public:
-//   BasePlannerNode();
-//   virtual ~BasePlannerNode(){};
-// };
-// } // namespace motion_planner
-
 #ifndef EB_PATH_PLANNER_H
 #define EB_PATH_PLANNER_H
 #include "motion_base_planner/node.hpp"
 
-// namespace autoware_msgs
-// {
-//   ROS_DECLARE_MESSAGE(Lane); 
-//   ROS_DECLARE_MESSAGE(Waypoint); 
-// }
+namespace autoware_lanelet2_msgs
+{
+  ROS_DECLARE_MESSAGE(MapBin); 
+}
 
 namespace geometry_msgs
 { 
   ROS_DECLARE_MESSAGE(PoseStamped);
-  // ROS_DECLARE_MESSAGE(Pose);
   ROS_DECLARE_MESSAGE(TwistStamped);
-  // ROS_DECLARE_MESSAGE(TransformStamped);
+}
+
+
+namespace lanelet
+{
+   class ConstLanelet;
+   using ConstLanelets = std::vector<ConstLanelet>;
 }
 
 namespace motion_planner
@@ -60,6 +36,7 @@ private:
   ros::Subscriber current_velocity_sub_;
   ros::Subscriber safety_waypoints_sub_;
   ros::Subscriber objects_sub_;
+  ros::Subscriber bin_map_sub_;
   
   
   ros::Timer timer_;
@@ -67,17 +44,20 @@ private:
   // std::unique_ptr<geometry_msgs::TransformStaped> gridmap2map_tf_;
   // std::unique_ptr<geometry_msgs::TransformStamped> lidar2map_tf_;
   // std::unique_ptr<geometry_msgs::TransformStamped> map2gridmap_tf_;
+  std::unique_ptr<lanelet::ConstLanelets> kept_road_lanelets_;
   
   // std::shared_ptr<autoware_msgs::Lane> in_waypoints_ptr_;
   // geometry_msgs::PoseStamped in_pose_ptr_;
   std::shared_ptr<geometry_msgs::PoseStamped> in_pose_ptr_;
   std::shared_ptr<geometry_msgs::TwistStamped> in_twist_ptr_;
+  std::shared_ptr<autoware_lanelet2_msgs::MapBin> in_map_ptr_;
   // std::shared_ptr<autoware_msgs::DetectedObjectArray> in_objects_ptr_;
   
   // void waypointsCallback(const autoware_msgs::Lane& msg);
   // void currentPoseCallback(const geometry_msgs::PoseStamped& msg);
   // void currentVelocityCallback(const geometry_msgs::TwistStamped& msg);
   // void objectsCallback(const autoware_msgs::DetectedObjectArray& msg);
+  void binMapCallback(const autoware_lanelet2_msgs::MapBin& msg);
   void timerCallback(const ros::TimerEvent &e);
   
 public:
--- src/planning/motion_planning/motion_base_planner/src/eb_path_planner/node.cpp
+++ src/planning/motion_planning/motion_base_planner/src/eb_path_planner/node.cpp
@@ -3,6 +3,16 @@
 #include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <autoware_lanelet2_msgs/MapBin.h>
+
+
+// #include <lanelet2_projection/UTM.h>
+// #include <lanelet2_core/LaneletMap.h>
+#include <lanelet2_core/Forward.h>
+#include <lanelet2_extension/utility/message_conversion.h>
+#include <lanelet2_extension/utility/query.h>
+#include <lanelet2_extension/visualization/visualization.h>
+#include <lanelet2_extension/regulatory_elements/autoware_traffic_light.h>
 
 #include <memory>
 
@@ -22,8 +32,9 @@ namespace motion_planner
     // current_pose_sub_ = nh_.subscribe("/current_pose", 1, &EBPathPlannerNode::currentPoseCallback, this);
     // current_velocity_sub_ = nh_.subscribe("/current_velocity", 1, &EBPathPlannerNode::currentVelocityCallback, this);
     // objects_sub_ = nh_.subscribe("/detection/lidar_detector/objects", 1, &QPPlannerROS::objectsCallback, this);
+    bin_map_sub_ = nh_.subscribe("/lanelet_map_bin", 1, &EBPathPlannerNode::binMapCallback,this);
     
-    // double timer_callback_dt = 0.05;
+    // double timer_callback_dt = 0.05;   
     double timer_callback_delta_second = 0.1;
     // double timer_callback_delta_second = 1.0;
     // double timer_callback_delta_second = 0.5;
@@ -55,6 +66,7 @@ namespace motion_planner
   
   void EBPathPlannerNode::timerCallback(const ros::TimerEvent &e)
   {
+    std::cerr << "aaaaa"  << std::endl;
     // if(!in_pose_ptr_)
     // {
     //   std::cerr << "pose not arrive" << std::endl;
@@ -73,6 +85,42 @@ namespace motion_planner
     //   in_waypoints_ptr_) 
     // { 
     //   final_waypoints_pub_.publish(*in_waypoints_ptr_);
+    // }
+  }
+  
+  void EBPathPlannerNode::binMapCallback(const autoware_lanelet2_msgs::MapBin& msg)
+  {
+    std::cerr << "map"  << std::endl;
+    lanelet::LaneletMapPtr viz_lanelet_map(new lanelet::LaneletMap);
+
+    lanelet::utils::conversion::fromBinMsg(msg, viz_lanelet_map);
+    ROS_INFO("Map is loaded\n");
+
+    // get lanelets etc to visualize
+    lanelet::ConstLanelets all_lanelets = lanelet::utils::query::laneletLayer(viz_lanelet_map);
+    lanelet::ConstLanelets road_lanelets = lanelet::utils::query::roadLanelets(all_lanelets);
+    kept_road_lanelets_.reset(new lanelet::ConstLanelets(road_lanelets));
+    // double lss = 0.2;  // line string size
+    // visualization_msgs::MarkerArray marker_array;
+    // for (auto li = lanelets.begin(); li != lanelets.end(); li++)
+    // {
+    //   lanelet::ConstLanelet lll = *li;
+
+    //   lanelet::ConstLineString3d left_ls = lll.leftBound();
+    //   lanelet::ConstLineString3d right_ls = lll.rightBound();
+    //   lanelet::ConstLineString3d center_ls = lll.centerline();
+
+    //   visualization_msgs::Marker left_line_strip, right_line_strip, center_line_strip;
+
+    //   visualization::lineString2Marker(left_ls, &left_line_strip, "map", "left_lane_bound", c, lss);
+    //   visualization::lineString2Marker(right_ls, &right_line_strip, "map", "right_lane_bound", c, lss);
+    //   marker_array.markers.push_back(left_line_strip);
+    //   marker_array.markers.push_back(right_line_strip);
+    //   if (viz_centerline)
+    //   {
+    //     visualization::lineString2Marker(center_ls, &center_line_strip, "map", "center_lane_line", c, lss * 0.5);
+    //     marker_array.markers.push_back(center_line_strip);
+    //   }
     // }
   }
 
