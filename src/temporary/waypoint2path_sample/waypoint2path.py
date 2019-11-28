e84e9aae056b9c6c7839f6327fa3a4b90ab6d6e3
Kosuke Murakami <kosuke.murakmi@tier4.jp>
2019-11-28 11:51:20 +0900

    add simple python converter from waypoint2path
    Signed-off-by: Kosuke Murakami <kosuke.murakmi@tier4.jp>

---
+++ src/temporary/waypoint2path_sample/waypoint2path.py
@@ -0,0 +1,11 @@
+import csv
+
+with open('/home/kosuke/Downloads/citypilot_waypoint.csv') as f:
+    reader = csv.reader(f)
+    l = [row for row in reader]
+    l.pop(0)
+with open('/home/kosuke/data/peoria/path.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(["x", "y", "z", "roll[rad]", "pitch[rad]", "yaw[rad]", "linear_velocity[m/s]", "angular_velocity[rad/s]", "linear_acceleration[m/s^2]", "angular_acceleration[rad/s^2]"])
+    for row in l:
+      writer.writerow([row[0],row[1], row[2], 0, 0, row[3], row[4], 0, 0, 0])
