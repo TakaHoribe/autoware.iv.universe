e3f70ce91728f2f3e1c551a83324a2c161166287
Kosuke Murakami <kosuke.murakami@tier4.jp>
2019-11-28 17:24:51 +0900

    add us_sample
    Signed-off-by: Kosuke Murakami <kosuke.murakami@tier4.jp>

--- src/temporary/waypoint2path_sample/waypoint2path.py
+++ src/temporary/waypoint2path_sample/waypoint2path.py
@@ -1,10 +1,10 @@
 import csv
 
-with open('/home/kosuke/Downloads/citypilot_waypoint.csv') as f:
+with open('/home/kosuke/Downloads/us_sample_waypoint.csv') as f:
     reader = csv.reader(f)
     l = [row for row in reader]
     l.pop(0)
-with open('/home/kosuke/data/peoria/path.csv', 'w') as f:
+with open('/home/kosuke/data/us_sample/path.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow(["x", "y", "z", "roll[rad]", "pitch[rad]", "yaw[rad]", "linear_velocity[m/s]", "angular_velocity[rad/s]", "linear_acceleration[m/s^2]", "angular_acceleration[rad/s^2]"])
     for row in l:
