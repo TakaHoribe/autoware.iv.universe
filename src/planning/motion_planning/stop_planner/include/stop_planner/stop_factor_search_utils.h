7a0522c4b0781703722673287fe1add862a6a2b6
Takamasa Horibe <horibe.takamasa@gmail.com>
2019-12-06 18:12:19 +0900

    add stop_planner
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

---
+++ src/planning/motion_planning/stop_planner/include/stop_planner/stop_factor_search_utils.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2015-2019 Autoware Foundation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+// ROS includes
+#include <geometry_msgs/PoseStamped.h>
+#include <ros/ros.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <sensor_msgs/PointCloud2.h>
+
+// User defined includes
+#include "autoware_planning_msgs/Trajectory.h"
+#include "stop_planner/planning_utils.h"
+
+
+// C++ includes
+#include <chrono>
+#include <memory>
+
+namespace motion_planner
+{
+    
+using PolygonX = std::vector<geometry_msgs::Point>;
+typedef std::pair<int32_t, int8_t> StopFactorPair; // idx, kind
+
+
+// display the next waypoint by markers.
+visualization_msgs::Marker displayWall(const geometry_msgs::Pose &pose, int8_t kind, int32_t id = 0);
+std::vector<geometry_msgs::Point> createLattice(const geometry_msgs::Pose &pose, double height, double width,
+                                                double count);
+
+std::unique_ptr<std_msgs::ColorRGBA> setColorDependsOnObstacleKind(int8_t kind);
+std_msgs::ColorRGBA setColorWhite();
+std_msgs::ColorRGBA setColorGray();
+std_msgs::ColorRGBA setColorYellow();
+
+std::pair<bool, int32_t> calcForwardIdxByLineIntegral(const autoware_planning_msgs::Trajectory &lane, int32_t base_idx, double stop_offset_dist);
+
+std::pair<bool, std::vector<geometry_msgs::Point>> findPointCloudInPolygon(const PolygonX &poly, const sensor_msgs::PointCloud2 &pc, int32_t points_thr);
+std::pair<bool, std::vector<geometry_msgs::Point>> findPointsInPolygon(const PolygonX &poly, const std::vector<geometry_msgs::Point> &points, int32_t points_thr = 1);
+
+
+geometry_msgs::Point calcClosestPointByXAxis(const geometry_msgs::Pose &pose, const std::vector<geometry_msgs::Point> &points);
+
+}  // namespace motion_planner
---
+++ src/planning/motion_planning/stop_planner/include/stop_planner/stop_planner_core.hpp
@@ -0,0 +1,70 @@
+
+/*
+ * Copyright 2018-2019 Autoware Foundation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MOTION_PLANNING_STOP_PLANNER_H
+#define MOTION_PLANNING_STOP_PLANNER_H
+
+#include <ros/ros.h>
+
+#include <autoware_planning_msgs/Trajectory.h>
+
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <autoware_planning_msgs/Trajectory.h>
+#include <tf2/utils.h>
+#include <tf2_ros/transform_listener.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include <tf2/convert.h>
+#include <tf2/transform_datatypes.h>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+
+#include "stop_planner/obstacle_considered_lane.h"
+
+class StopPlanner
+{
+public:
+  StopPlanner();
+  ~StopPlanner();
+
+private:
+  void trajecotyCallback();
+
+  // ros
+  ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
+  ros::Publisher trajectory_pub_;
+  ros::Publisher marker_viz_pub_;
+
+  ros::Subscriber trajectory_sub_;
+  ros::Subscriber obstacle_pcd_sub_;
+
+  tf2_ros::Buffer tf_buffer_;              //!< @brief tf buffer for current_pose
+  tf2_ros::TransformListener tf_listener_; //!< @brief tf listener for current_pose
+
+  std::shared_ptr<sensor_msgs::PointCloud2> in_obstacle_pcd_ptr_;
+  std::shared_ptr<autoware_planning_msgs::Trajectory> in_trajectory_ptr_;
+  std::shared_ptr<geometry_msgs::PoseStamped> current_pose_ptr_;
+
+  motion_planner::ObstacleConsideredLane obstacle_pcd_stop_planner_;
+
+  void callbackTrajectory(const autoware_planning_msgs::Trajectory::ConstPtr &msg);
+  void callbackPointCloud(const sensor_msgs::PointCloud2::ConstPtr &msg);
+  bool updateCurrentPose(const double timeout);
+};
+
+#endif
