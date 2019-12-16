463fdd0b1f8ab84ac022e8f383d1466d3e639ce0
Takamasa Horibe <horibe.takamasa@gmail.com>
2019-12-16 15:04:05 +0900

    fix build warning in accel_map_converter
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- src/vehicle/as/nodes/lexus_converter/accel_map.cpp
+++ src/vehicle/as/nodes/lexus_converter/accel_map.cpp
@@ -31,13 +31,13 @@ bool AccelMap::readAccelMapFromCSV(std::string csv_path)
 
   if (!csv.readCsv(table))
   {
-    ROS_ERROR("[Accel Map] Cannot open %s", csv_path);
+    ROS_ERROR("[Accel Map] Cannot open %s", csv_path.c_str());
     return false;
   }
 
   if (table[0].size() < 2)
   {
-    ROS_ERROR("[Accel Map] Cannot read %s. CSV file should have at least 2 column", csv_path);
+    ROS_ERROR("[Accel Map] Cannot read %s. CSV file should have at least 2 column", csv_path.c_str());
     return false;
   }
   vehicle_name_ = table[0][0];
@@ -50,7 +50,7 @@ bool AccelMap::readAccelMapFromCSV(std::string csv_path)
   {
     if (table[0].size() != table[i].size())
     {
-      ROS_ERROR("[Accel Map] Cannot read %s. Each row should have a same number of columns", csv_path);
+      ROS_ERROR("[Accel Map] Cannot read %s. Each row should have a same number of columns", csv_path.c_str());
       return false;
     }
     throttle_index_.push_back(std::stod(table[i][0]));
