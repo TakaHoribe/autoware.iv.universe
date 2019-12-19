0bacea06a76d289ee10ad748f983356014f80f60
Kenji Miyake <kenji.miyake@tier4.jp>
2019-12-19 20:42:30 +0900

    Speed up align_vector_map_to_pointcloud.py
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- src/map/autoware_vector_map/scripts/autoware_vector_map/align_vector_map_to_pointcloud.py
+++ src/map/autoware_vector_map/scripts/autoware_vector_map/align_vector_map_to_pointcloud.py
@@ -9,20 +9,25 @@ from shapely.geometry import LinearRing, LineString, Point, Polygon
 from autoware_vector_map.map_api import MapApi
 
 
-def find_nearest_z_value(points, x, y, z):
-    # Alias
+def filter_points_by_rectangle(points, x_min, x_max, y_min, y_max):
     xs = points[:, 0]
     ys = points[:, 1]
-    zs = points[:, 2]
 
-    # Filter by rectangle
+    idx = np.where((x_min < xs) & (xs < x_max) & (y_min < ys) & (ys < y_max))
+
+    return points[idx[0]]
+
+
+def find_nearest_z_value(points, x, y, z):
     margin = 5
-    idx = np.argwhere((x - margin < xs) & (xs < x + margin) & (y - margin < ys) & (ys < y + margin))
+    x_min, x_max = (x - margin, x + margin)
+    y_min, y_max = (y - margin, y + margin)
+    filtered_points = filter_points_by_rectangle(points, x_min, x_max, y_min, y_max)
 
-    if not idx.any():
+    if not filtered_points.any():
         return None
 
-    new_z = np.min(zs[idx])
+    new_z = np.min(filtered_points[:, 2])
 
     return new_z
 
@@ -30,33 +35,40 @@ def find_nearest_z_value(points, x, y, z):
 def create_new_coord(points, coord):
     x, y, z = (coord[0], coord[1], coord[2])
     new_z = find_nearest_z_value(points, x, y, z)
-    return (x, y, new_z)
+
+    if new_z:
+        return (x, y, new_z)
+    else:
+        return (x, y, z)
 
 
 def create_new_geometry(points, geometry):
     if geometry.geom_type == "Point":
         return create_new_coord(points, geometry.coords[0])
 
+    x_min, y_min, x_max, y_max = geometry.bounds
+    filtered_points = filter_points_by_rectangle(points, x_min, x_max, y_min, y_max)
+
     if geometry.geom_type == "LineString":
         new_coords = []
         for coord in geometry.coords:
-            new_coords.append(create_new_coord(points, coord))
+            new_coords.append(create_new_coord(filtered_points, coord))
 
         return LineString(new_coords)
 
     if geometry.geom_type == "LinearRing":
         new_coords = []
         for coord in geometry.coords:
-            new_coords.append(create_new_coord(points, coord))
+            new_coords.append(create_new_coord(filtered_points, coord))
 
         return LinearRing(new_coords)
 
     if geometry.geom_type == "Polygon":
-        new_exterior = create_new_geometry(points, geometry.exterior).coords
+        new_exterior = create_new_geometry(filtered_points, geometry.exterior).coords
 
         new_interiors = []
         for interior in geometry.interiors:
-            new_interiors.append(create_new_geometry(points, interior).coords)
+            new_interiors.append(create_new_geometry(filtered_points, interior).coords)
 
         return Polygon(new_exterior, new_interiors)
 
@@ -67,8 +79,11 @@ def align_features(map_api, table_name, points):
     if not gdf.geometry.any():
         return
 
+    x_min, y_min, x_max, y_max = gdf.total_bounds
+    filtered_points = filter_points_by_rectangle(points, x_min, x_max, y_min, y_max)
+
     for i, row in gdf.iterrows():
-        gdf.loc[i, "geometry"] = create_new_geometry(points, row.geometry)
+        gdf.loc[i, "geometry"] = create_new_geometry(filtered_points, row.geometry)
 
     map_api.save_gdf(table_name, gdf)
 
