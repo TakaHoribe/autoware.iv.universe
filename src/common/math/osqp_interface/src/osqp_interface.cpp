14da9aed1df09d41b7049c8a481b0a5a0b813658
Takamasa Horibe <horibe.takamasa@gmail.com>
2019-12-20 19:28:57 +0900

    modify osqp param
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- src/common/math/osqp_interface/src/osqp_interface.cpp
+++ src/common/math/osqp_interface/src/osqp_interface.cpp
@@ -83,10 +83,12 @@ std::tuple<std::vector<float>, std::vector<float>> optimize(const Eigen::MatrixX
   // Define Solver settings as default
   osqp_set_default_settings(settings);
   settings->alpha = 1.0;  // Change alpha parameter
-  settings->eps_rel = 1.0E-6;
-  settings->eps_abs = 1.0E-6;
-  settings->eps_prim_inf = 1.0E-6;
-  settings->eps_dual_inf = 1.0E-6;
+  settings->eps_rel = 1.0E-4;
+  settings->eps_abs = 1.0E-4;
+  settings->eps_prim_inf = 1.0E-4;
+  settings->eps_dual_inf = 1.0E-4;
+  settings->warm_start = true;
+  settings->max_iter = 4000;
 
 
   // Setup workspace
