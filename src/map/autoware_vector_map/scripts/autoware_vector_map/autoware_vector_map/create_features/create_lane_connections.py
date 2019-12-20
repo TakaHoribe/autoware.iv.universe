6b5f95bcaf3f28f5231a7a49fa298477e90506d1
Kenji Miyake <kenji.miyake@tier4.jp>
2019-12-21 01:49:37 +0900

    Change lane connecting condition to distance
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- src/map/autoware_vector_map/scripts/autoware_vector_map/autoware_vector_map/create_features/create_lane_connections.py
+++ src/map/autoware_vector_map/scripts/autoware_vector_map/autoware_vector_map/create_features/create_lane_connections.py
@@ -1,3 +1,6 @@
+from shapely.geometry import Point
+
+
 def create_lane_connection(lane_id, next_lane_id):
     return {"geometry": None, "properties": {"lane_id": lane_id, "next_lane_id": next_lane_id}}
 
@@ -9,7 +12,11 @@ def create_lane_connections(map_api):
     for base_lane in lanes.itertuples():
         touch_lanes = lanes[lanes.geometry.touches(base_lane.geometry)]
         for touch_lane in touch_lanes.itertuples():
-            if base_lane.geometry.coords[-1] == touch_lane.geometry.coords[0]:
+            p_end = base_lane.geometry.coords[-1]
+            p_start = touch_lane.geometry.coords[0]
+
+            th_dist = 0.01
+            if Point(p_end).distance(Point(p_start)) < th_dist:
                 lane_connections.append(create_lane_connection(base_lane.id, touch_lane.id))
 
     return lane_connections
