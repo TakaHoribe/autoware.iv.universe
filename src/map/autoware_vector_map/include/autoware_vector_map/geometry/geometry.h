00ab3252709c9eb1c1d11d2514e019b95769dd43
Kenji Miyake <kenji.miyake@tier4.jp>
2019-12-21 02:46:18 +0900

    Add intersection function
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- src/map/autoware_vector_map/include/autoware_vector_map/geometry/geometry.h
+++ src/map/autoware_vector_map/include/autoware_vector_map/geometry/geometry.h
@@ -1,11 +1,17 @@
 #pragma once
 
-#include <autoware_vector_map/bridge/boost/boost_geometry_bridge.h>
+#include <vector>
+
+#include <autoware_vector_map/core.h>
 
 namespace autoware_vector_map {
 namespace geometry {
 
-//
+std::vector<Point3d> intersection(const LineString3d& geom1, const LineString3d& geom2);
+
+std::vector<Point2d> intersection(const LineString2d& geom1, const LineString2d& geom2);
+std::vector<Polygon2d> intersection(const LinearRing2d& geom1, const LinearRing2d& geom2);
+std::vector<Polygon2d> intersection(const Polygon2d& geom1, const Polygon2d& geom2);
 
 }  // namespace geometry
 }  // namespace autoware_vector_map
