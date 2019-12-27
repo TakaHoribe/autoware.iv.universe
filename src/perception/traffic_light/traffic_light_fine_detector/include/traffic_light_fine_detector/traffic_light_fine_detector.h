f0d41bf8eba2a5f5dae150d9cde6c960e3ede3c8
wep21 <border_goldenmarket@yahoo.co.jp>
2019-12-27 11:43:20 +0900

    Modify traffic light fine detector
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>

--- src/perception/traffic_light/traffic_light_fine_detector/include/traffic_light_fine_detector/traffic_light_fine_detector.h
+++ src/perception/traffic_light/traffic_light_fine_detector/include/traffic_light_fine_detector/traffic_light_fine_detector.h
@@ -8,7 +8,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv/cv.hpp>
 
-#include "new_trafficlight_recognizer/utils.h"
 #include "TrtNet.h"
 #include "data_reader.h"
 
@@ -23,16 +22,16 @@
 #include <std_msgs/Header.h>
 #include <cv_bridge/cv_bridge.h>
 
-#include <autoware_traffic_light_msgs/StampedRoi.h>
+#include <autoware_traffic_light_msgs/TrafficLightRoiArray.h>
 
 typedef struct Detection
 {
   float x, y, w, h, prob;
 } Detection;
 
-namespace new_trafficlight_recognizer
+namespace traffic_light
 {
-class TrafficLightDetectorNode
+class TrafficLightFineDetectorNode
 {
 private:
   // variables
@@ -40,23 +39,21 @@ private:
   ros::NodeHandle nh_;
   ros::NodeHandle pnh_;
 
-  typedef message_filters::sync_policies::ExactTime<sensor_msgs::Image, autoware_traffic_light_msgs::StampedRoi>
+  typedef message_filters::sync_policies::ExactTime<sensor_msgs::Image, autoware_traffic_light_msgs::TrafficLightRoiArray>
       SyncPolicy;
 
-  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, autoware_traffic_light_msgs::StampedRoi>
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, autoware_traffic_light_msgs::TrafficLightRoiArray>
       ApproximateSyncPolicy;
 
   std::shared_ptr<message_filters::Synchronizer<SyncPolicy> > sync_;
   std::shared_ptr<message_filters::Synchronizer<ApproximateSyncPolicy> > approximate_sync_;
   message_filters::Subscriber<sensor_msgs::Image> image_sub_;
-  message_filters::Subscriber<autoware_traffic_light_msgs::StampedRoi> stamped_roi_sub_;
+  message_filters::Subscriber<autoware_traffic_light_msgs::TrafficLightRoiArray> traffic_light_roi_sub_;
   ros::Publisher output_rois_pub_;
-  ros::Publisher output_debug_pub_;
 
   bool is_approximate_sync_;
   double score_thresh_;
 
-  std::shared_ptr<Utils> utils_ptr_;
   std::unique_ptr<Tn::trtNet> net_ptr_;
   std::vector<std::vector<int> > output_shape = { { 1, 18, 8, 8 }, { 1, 18, 16, 16 }, { 1, 18, 32, 32 } };
   std::vector<std::vector<int> > g_masks = { { 6, 7, 8 }, { 3, 4, 5 }, { 0, 1, 2 } };
@@ -67,7 +64,7 @@ private:
   void initROS();
 
   void callback(const sensor_msgs::Image::ConstPtr& image_msg,
-                const autoware_traffic_light_msgs::StampedRoi::ConstPtr& stamped_roi_msg);
+                const autoware_traffic_light_msgs::TrafficLightRoiArray::ConstPtr& traffic_light_roi_msg);
 
   inline float sigmoid(float in)
   {
@@ -77,13 +74,15 @@ private:
   std::vector<float> prepareImage(cv::Mat& in_img);
   std::vector<Tn::Bbox> postProcessImg(float* output, const int classes, cv::Mat& img);
   void doNms(std::vector<Detection>& detections, float nms_thresh);
-
+  bool rosmsg2cvmat(const sensor_msgs::Image::ConstPtr& image_msg, cv::Mat& image);
+  bool fit_in_frame(cv::Point& lt, cv::Point& rb, const cv::Size& size);
+  void cvrect2tlroimsg(const cv::Rect& rect, const int32_t id, autoware_traffic_light_msgs::TrafficLightRoi& tl_roi);
 public:
-  TrafficLightDetectorNode();
-  ~TrafficLightDetectorNode();
+  TrafficLightFineDetectorNode();
+  ~TrafficLightFineDetectorNode();
   void run();
 
-};  // TrafficLightDetector
+};  // TrafficLightFineDetector
 
 }  // namespace
 
---
+++ src/perception/traffic_light/traffic_light_fine_detector/src/traffic_light_fine_detector_main.cpp
@@ -0,0 +1,11 @@
+#include "traffic_light_fine_detector/traffic_light_fine_detector.h"
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "traffic_light_fine_detector");
+
+  traffic_light::TrafficLightFineDetectorNode node;
+  node.run();
+
+  return 0;
+}
