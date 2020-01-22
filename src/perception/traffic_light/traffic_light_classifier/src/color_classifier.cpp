f6d08e246c3fffeb9ca043c92254af0ea9948fff
Yukihiro Saito <yukky.saito@gmail.com>
2020-01-22 13:18:25 +0900

    fixed bug

--- src/perception/traffic_light/traffic_light_classifier/src/color_classifier.cpp
+++ src/perception/traffic_light/traffic_light_classifier/src/color_classifier.cpp
@@ -104,28 +104,28 @@ bool ColorClassifier::getLampState(const cv::Mat &input_image, std::vector<autow
     {
         autoware_traffic_light_msgs::LampState state;
         state.type = autoware_traffic_light_msgs::LampState::GREEN;
-        state.confidence = green_ratio;
+        state.confidence = double(green_pixel_num) / (20.0 * 20.0);
         states.push_back(state);
     }
     else if (green_ratio < yellow_ratio && red_ratio < yellow_ratio)
     {
         autoware_traffic_light_msgs::LampState state;
         state.type = autoware_traffic_light_msgs::LampState::YELLOW;
-        state.confidence = yellow_ratio;
+        state.confidence = double(yellow_pixel_num) / (20.0 * 20.0);
         states.push_back(state);
     }
     else if (green_ratio < red_ratio && yellow_ratio < red_ratio)
     {
         autoware_traffic_light_msgs::LampState state;
         state.type = autoware_traffic_light_msgs::LampState::RED;
-        state.confidence = red_ratio;
+        state.confidence = double(red_pixel_num) / (20.0 * 20.0);
         states.push_back(state);
     }
     else
     {
         autoware_traffic_light_msgs::LampState state;
         state.type = autoware_traffic_light_msgs::LampState::UNKNOWN;
-        state.confidence = 1.0;
+        state.confidence = 0.0;
         states.push_back(state);
     }
     return true;
