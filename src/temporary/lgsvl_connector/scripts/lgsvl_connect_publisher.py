8f987810c8cfe075c7c7b265156f48f6e76fdc83
tomoya.kimura <tomoya.kimura@tier4.jp>
2020-01-29 15:55:22 +0900

    deal with back

--- src/temporary/lgsvl_connector/scripts/lgsvl_connect_publisher.py
+++ src/temporary/lgsvl_connector/scripts/lgsvl_connect_publisher.py
@@ -11,7 +11,7 @@ import numpy as np
 import rospy
 import tf
 from autoware_msgs.msg import VehicleCmd
-from autoware_vehicle_msgs.msg import Steering, VehicleCommandStamped
+from autoware_vehicle_msgs.msg import Steering, VehicleCommandStamped, Shift
 from geometry_msgs.msg import TwistStamped
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image, PointCloud2
@@ -20,18 +20,30 @@ WHEEL_BASE = 2.9  # [m]
 CAMERA_LINK = "traffic_light/camera_link"
 
 
+# geer
+REVERSE = 2
+DRIVE = 4
+
+
 class LgsvlConnectPublisher:
     def __init__(self):
         self.v_offset_rate = rospy.get_param("v_offset_rate", 1.0)  # [rate] actual_velocity/velocity_from_simulator
-        self.w_offset_rate = rospy.get_param("w_offset_rate", 1.0)  # [rate] actual_angular_velocity/angular_velocity_from_simulator
-        self.accel_command_offset_rate = rospy.get_param("accel_offset_rate", 0.22) #[rate] accel command/accel behavior in simulator
-        self.steer_command_offset_rate = rospy.get_param("steer_offset_rate", 0.28) #[rate] steer command/steer behavior in simulator
+        self.w_offset_rate = rospy.get_param(
+            "w_offset_rate", 1.0
+        )  # [rate] actual_angular_velocity/angular_velocity_from_simulator
+        self.accel_command_offset_rate = rospy.get_param(
+            "accel_offset_rate", 0.22
+        )  # [rate] accel command/accel behavior in simulator
+        self.steer_command_offset_rate = rospy.get_param(
+            "steer_offset_rate", 0.28
+        )  # [rate] steer command/steer behavior in simulator
         self.max_steering_velocity = rospy.get_param("max_steering_velocity", 2.0)  # [rad/s]
-        self.throttle_brake_mode = rospy.get_param("throttle_brake_mode", False)  # [rad/s]
+        self.throttle_brake_mode = rospy.get_param("throttle_brake_mode", True)  # [rad/s]
 
         self.v = 0.0
         self.last_steer = 0.0
         self.last_command_time = rospy.Time.now().to_sec()
+        self.shift = DRIVE
         self.tfb = tf.TransformBroadcaster()
         self.tfl = tf.TransformListener()
 
@@ -47,6 +59,9 @@ class LgsvlConnectPublisher:
         self.pubcmd = rospy.Publisher("/vehicle_cmd", VehicleCmd, queue_size=1)
         self.subcmd = rospy.Subscriber(
             "/control/vehicle_cmd", VehicleCommandStamped, self.CallBackVehicleCmd, queue_size=1, tcp_nodelay=True
+        )
+        self.subshidt = rospy.Subscriber(
+            "/vehicle/shift_cmd", Shift, self.CallBackShift, queue_size=1, tcp_nodelay=True
         )
 
         # for Problem4: publish /vehicle/status/steering
@@ -95,6 +110,9 @@ class LgsvlConnectPublisher:
         pclmsg.header.stamp = rospy.Time.now()
         self.pubpcl.publish(pclmsg)
 
+    def CallBackShift(self, shiftmsg):
+        self.shift = shiftmsg.data
+
     def CallBackVehicleCmd(
         self, vcmdmsg
     ):  # Problem3: The output msg type of autoware is different from the input msg type of LG-simulator
@@ -114,20 +132,23 @@ class LgsvlConnectPublisher:
         # Angular Velocity
         if self.v != 0:
             # cmdmsg.twist_cmd.twist.angular.z = math.tan(vcmdmsg.command.control.steering_angle)
-            cmdmsg.twist_cmd.twist.angular.z = steer * (self.steer_command_offset_rate*2.0) / self.w_offset_rate
+            cmdmsg.twist_cmd.twist.angular.z = steer * (self.steer_command_offset_rate * 2.0) / self.w_offset_rate
         else:
             cmdmsg.twist_cmd.twist.angular.z = 0.0
 
         cmdmsg.ctrl_cmd.linear_velocity = vcmdmsg.command.control.velocity  # Velocity(no use)
-        cmdmsg.ctrl_cmd.linear_acceleration = vcmdmsg.command.control.acceleration*self.accel_command_offset_rate  # accel
-        cmdmsg.ctrl_cmd.steering_angle = -steer*self.steer_command_offset_rate  # steer
+        cmdmsg.ctrl_cmd.linear_acceleration = (
+            vcmdmsg.command.control.acceleration * self.accel_command_offset_rate
+        )  # accel
+        cmdmsg.ctrl_cmd.steering_angle = -steer * self.steer_command_offset_rate  # steer
         cmdmsg.emergency = vcmdmsg.command.emergency
 
         if self.throttle_brake_mode:
-            if vcmdmsg.command.control.velocity >=0:
-                cmdmsg.gear = 64#drive
+            if self.shift == REVERSE:
+                cmdmsg.gear = 63  # reverse
+                cmdmsg.ctrl_cmd.linear_acceleration *= -1  # reverse accel
             else:
-                cmdmsg.gear = 63#reverse
+                cmdmsg.gear = 64  # drive
 
         self.pubcmd.publish(cmdmsg)
 
