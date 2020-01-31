4a300618f33fb8d18ec2f169f90682f9f0eec576
Takamasa Horibe <horibe.takamasa@gmail.com>
2020-01-31 21:17:00 +0900

    fix steering & velocity rviz plugin
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- src/common/util/rviz_plugins/autoware_vehicle_rviz_plugin/src/tools/console_meter.hpp
+++ src/common/util/rviz_plugins/autoware_vehicle_rviz_plugin/src/tools/console_meter.hpp
@@ -29,7 +29,7 @@
 namespace rviz_plugins
 {
 
-class ConsoleMeterDisplay : public rviz::MessageFilterDisplay<autoware_vehicle_msgs::VehicleStatusStamped>
+class ConsoleMeterDisplay : public rviz::MessageFilterDisplay<geometry_msgs::TwistStamped>
 {
   Q_OBJECT
 
@@ -45,7 +45,7 @@ private Q_SLOTS:
   void updateVisualization();
 
 protected:
-  void processMessage(const autoware_vehicle_msgs::VehicleStatusStampedConstPtr &msg_ptr) override;
+  void processMessage(const geometry_msgs::TwistStampedConstPtr &msg_ptr) override;
   std::unique_ptr<Ogre::ColourValue> setColorDependsOnVelocity(const double vel_max, const double cmd_vel);
   std::unique_ptr<Ogre::ColourValue> gradation(const QColor &color_min, const QColor &color_max,
                                                const double ratio);
@@ -63,7 +63,7 @@ private:
   const double meter_max_velocity_;
   const double meter_min_angle_;
   const double meter_max_angle_;
-  autoware_vehicle_msgs::VehicleStatusStampedConstPtr last_msg_ptr_;
+  geometry_msgs::TwistStampedConstPtr last_msg_ptr_;
 };
 
 } // namespace rviz_plugins
