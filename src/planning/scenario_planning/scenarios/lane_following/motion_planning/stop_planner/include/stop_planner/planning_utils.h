3f1d96f07032762a7812d5e75c25da4dc74584ac
Takamasa Horibe <horibe.takamasa@gmail.com>
2020-02-06 15:00:36 +0900

    fix bug for circle detection range
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- src/planning/scenario_planning/scenarios/lane_following/motion_planning/stop_planner/include/stop_planner/planning_utils.h
+++ src/planning/scenario_planning/scenarios/lane_following/motion_planning/stop_planner/include/stop_planner/planning_utils.h
@@ -34,8 +34,7 @@
 // C++ includes
 #include <memory>
 
-namespace planning_utils
-{
+namespace planning_utils {
 
 constexpr double ERROR = 1e-6;
 
@@ -44,8 +43,8 @@ double calcDistSquared2D(const geometry_msgs::Point &p, const geometry_msgs::Poi
 double calcLateralError2D(const geometry_msgs::Point &a_start, const geometry_msgs::Point &a_end,
                           const geometry_msgs::Point &b);
 bool findClosestIdxWithDistAngThr(const autoware_planning_msgs::Trajectory &in_trajectory,
-                                  const geometry_msgs::Pose &curr_pose, int32_t &out_idx,
-                                  double dist_thr = 3.0, double angle_thr = M_PI_2); // TODO: more test
+                                  const geometry_msgs::Pose &curr_pose, int32_t &out_idx, double dist_thr = 3.0,
+                                  double angle_thr = M_PI_2);  // TODO: more test
 
 // refer from apache's pointinpoly in http://www.visibone.com/inpoly/
 template <typename T>
@@ -53,20 +52,26 @@ bool isInPolygon(const std::vector<T> &polygon, const T &point);
 template <>
 bool isInPolygon(const std::vector<geometry_msgs::Point> &polygon, const geometry_msgs::Point &point);
 
-geometry_msgs::Point transformToAbsoluteCoordinate2D(const geometry_msgs::Point &point, const geometry_msgs::Pose &current_pose);
-geometry_msgs::Point transformToAbsoluteCoordinate3D(const geometry_msgs::Point &point, const geometry_msgs::Pose &origin); // TODO: test
-geometry_msgs::Point transformToRelativeCoordinate2D(const geometry_msgs::Point &point, const geometry_msgs::Pose &current_pose);
-geometry_msgs::Point transformToRelativeCoordinate3D(const geometry_msgs::Point &point, const geometry_msgs::Pose &current_pose); // TODO: test
+geometry_msgs::Point transformToAbsoluteCoordinate2D(const geometry_msgs::Point &point,
+                                                     const geometry_msgs::Pose &current_pose);
+geometry_msgs::Point transformToAbsoluteCoordinate3D(const geometry_msgs::Point &point,
+                                                     const geometry_msgs::Pose &origin);  // TODO: test
+geometry_msgs::Point transformToRelativeCoordinate2D(const geometry_msgs::Point &point,
+                                                     const geometry_msgs::Pose &current_pose);
+geometry_msgs::Point transformToRelativeCoordinate3D(const geometry_msgs::Point &point,
+                                                     const geometry_msgs::Pose &current_pose);  // TODO: test
 geometry_msgs::Quaternion getQuaternionFromYaw(const double &_yaw);
 
 double normalizeEulerAngle(double euler);
 void convertEulerAngleToMonotonic(std::vector<double> &a);
-bool linearInterpTrajectory(const std::vector<double> &base_index, const autoware_planning_msgs::Trajectory &base_trajectory,
+bool linearInterpTrajectory(const std::vector<double> &base_index,
+                            const autoware_planning_msgs::Trajectory &base_trajectory,
                             const std::vector<double> &out_index, autoware_planning_msgs::Trajectory &out_trajectory);
-int calcForwardIdxByLineIntegral(const autoware_planning_msgs::Trajectory &in_trajectory, int32_t start_idx, double distance);
+int calcForwardIdxByLineIntegral(const autoware_planning_msgs::Trajectory &in_trajectory, int32_t start_idx,
+                                 double distance);
 
 geometry_msgs::Pose getPoseOnTrajectoryWithRadius(const autoware_planning_msgs::Trajectory &in_tajectory,
-                                                  const geometry_msgs::Point &origin,
-                                                  const int start_idx, const double distance);
+                                                  const geometry_msgs::Point &origin, const int start_idx,
+                                                  const double distance);
 
-} // namespace planning_utils
+}  // namespace planning_utils
