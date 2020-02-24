112c89a22a3aeb6b2b0f97a62c55573af640d621
Yukihiro Saito <yukky.saito@gmail.com>
2020-02-24 21:45:22 +0900

    update visualize

--- src/planning/scenario_planning/scenarios/lane_following/behavior_planning/behavior_velocity_planner/src/scene_module/traffic_light/debug_marker.cpp
+++ src/planning/scenario_planning/scenarios/lane_following/behavior_planning/behavior_velocity_planner/src/scene_module/traffic_light/debug_marker.cpp
@@ -1,5 +1,6 @@
 #include <behavior_velocity_planner/api.hpp>
 #include <scene_module/traffic_light/debug_marker.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 namespace behavior_planning {
 // TrafficLightDebugMarkersManager::TrafficLightDebugMarkersManager() : nh_(), pnh_("~"),
@@ -89,8 +90,11 @@ void MapBasedDetector::publishVisibleTrafficLights(const geometry_msgs::PoseStam
 void TrafficLightDebugMarkersManager::publish() {
   visualization_msgs::MarkerArray msg;
   ros::Time current_time = ros::Time::now();
+  double base_link2front;
+  getBaselink2FrontLength(base_link2front);
+  tf2::Transform tf_base_link2front(tf2::Quaternion(0.0, 0.0, 0.0, 1.0), tf2::Vector3(base_link2front, 0.0, 0.0));
 
-  // Geofence Stop
+  // Stop Geofence
   for (size_t j = 0; j < stop_poses_.size(); ++j) {
     visualization_msgs::Marker marker;
     marker.header.frame_id = "map";
@@ -98,15 +102,13 @@ void TrafficLightDebugMarkersManager::publish() {
     marker.ns = "stop geofence";
     marker.id = j;
     marker.lifetime = ros::Duration(0.5);
-    marker.type = visualization_msgs::Marker::CUBE_LIST;
+    marker.type = visualization_msgs::Marker::CUBE;
     marker.action = visualization_msgs::Marker::ADD;
-    marker.pose.position.x = stop_poses_.at(j).position.x;
-    marker.pose.position.y = stop_poses_.at(j).position.y;
-    marker.pose.position.z = stop_poses_.at(j).position.z + 1.0;
-    marker.pose.orientation.x = stop_poses_.at(j).orientation.x;
-    marker.pose.orientation.y = stop_poses_.at(j).orientation.y;
-    marker.pose.orientation.z = stop_poses_.at(j).orientation.z;
-    marker.pose.orientation.w = stop_poses_.at(j).orientation.w;
+    tf2::Transform tf_map2base_link;
+    tf2::fromMsg(stop_poses_.at(j), tf_map2base_link);
+    tf2::Transform tf_map2front = tf_map2base_link * tf_base_link2front;
+    tf2::toMsg(tf_map2front, marker.pose);
+    marker.pose.position.z += 1.0;
     marker.scale.x = 0.1;
     marker.scale.y = 5.0;
     marker.scale.z = 2.0;
@@ -114,16 +116,34 @@ void TrafficLightDebugMarkersManager::publish() {
     marker.color.r = 1.0;
     marker.color.g = 0.0;
     marker.color.b = 0.0;
-    double base_link2front;
-    getBaselink2FrontLength(base_link2front);
-    geometry_msgs::Point point;
-    point.x = base_link2front;
-    point.y = 0;
-    point.z = 0;
-    marker.points.push_back(point);
     msg.markers.push_back(marker);
   }
-  // Geofence Judge
+  // Facto Text
+  for (size_t j = 0; j < stop_poses_.size(); ++j) {
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "map";
+    marker.header.stamp = current_time;
+    marker.ns = "factor text";
+    marker.id = j;
+    marker.lifetime = ros::Duration(0.5);
+    marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+    marker.action = visualization_msgs::Marker::ADD;
+    tf2::Transform tf_map2base_link;
+    tf2::fromMsg(stop_poses_.at(j), tf_map2base_link);
+    tf2::Transform tf_map2front = tf_map2base_link * tf_base_link2front;
+    tf2::toMsg(tf_map2front, marker.pose);
+    marker.pose.position.z += 2.0;
+    marker.scale.x = 0.0;
+    marker.scale.y = 0.0;
+    marker.scale.z = 1.0;
+    marker.color.a = 0.999;  // Don't forget to set the alpha!
+    marker.color.r = 1.0;
+    marker.color.g = 1.0;
+    marker.color.b = 1.0;
+    marker.text = "traffic light";
+    msg.markers.push_back(marker);
+  }
+  // Judge Geofence
   for (size_t j = 0; j < judge_poses_.size(); ++j) {
     visualization_msgs::Marker marker;
     marker.header.frame_id = "map";
@@ -131,29 +151,45 @@ void TrafficLightDebugMarkersManager::publish() {
     marker.ns = "judge geofence";
     marker.id = j;
     marker.lifetime = ros::Duration(0.5);
-    marker.type = visualization_msgs::Marker::CUBE_LIST;
+    marker.type = visualization_msgs::Marker::CUBE;
     marker.action = visualization_msgs::Marker::ADD;
-    marker.pose.position.x = judge_poses_.at(j).position.x;
-    marker.pose.position.y = judge_poses_.at(j).position.y;
-    marker.pose.position.z = judge_poses_.at(j).position.z + 1.0;
-    marker.pose.orientation.x = judge_poses_.at(j).orientation.x;
-    marker.pose.orientation.y = judge_poses_.at(j).orientation.y;
-    marker.pose.orientation.z = judge_poses_.at(j).orientation.z;
-    marker.pose.orientation.w = judge_poses_.at(j).orientation.w;
+    tf2::Transform tf_map2base_link;
+    tf2::fromMsg(judge_poses_.at(j), tf_map2base_link);
+    tf2::Transform tf_map2front = tf_map2base_link * tf_base_link2front;
+    tf2::toMsg(tf_map2front, marker.pose);
+    marker.pose.position.z += 1.0;
     marker.scale.x = 0.1;
     marker.scale.y = 5.0;
     marker.scale.z = 2.0;
     marker.color.a = 0.5;  // Don't forget to set the alpha!
-    marker.color.r = 1.0;
-    marker.color.g = 0.0;
+    marker.color.r = 0.0;
+    marker.color.g = 1.0;
     marker.color.b = 0.0;
-    double base_link2front;
-    getBaselink2FrontLength(base_link2front);
-    geometry_msgs::Point point;
-    point.x = base_link2front;
-    point.y = 0;
-    point.z = 0;
-    marker.points.push_back(point);
+    msg.markers.push_back(marker);
+  }
+  // Facto Text
+  for (size_t j = 0; j < judge_poses_.size(); ++j) {
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "map";
+    marker.header.stamp = current_time;
+    marker.ns = "judge factor text";
+    marker.id = j;
+    marker.lifetime = ros::Duration(0.5);
+    marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+    marker.action = visualization_msgs::Marker::ADD;
+    tf2::Transform tf_map2base_link;
+    tf2::fromMsg(judge_poses_.at(j), tf_map2base_link);
+    tf2::Transform tf_map2front = tf_map2base_link * tf_base_link2front;
+    tf2::toMsg(tf_map2front, marker.pose);
+    marker.pose.position.z += 2.0;
+    marker.scale.x = 0.0;
+    marker.scale.y = 0.0;
+    marker.scale.z = 1.0;
+    marker.color.a = 0.999;  // Don't forget to set the alpha!
+    marker.color.r = 1.0;
+    marker.color.g = 1.0;
+    marker.color.b = 1.0;
+    marker.text = "traffic light (judge)";
     msg.markers.push_back(marker);
   }
 
