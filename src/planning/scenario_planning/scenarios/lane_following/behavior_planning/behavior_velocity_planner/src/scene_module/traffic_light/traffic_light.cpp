4907ddc27a324411febd8680216453c4da0f6ff9
tomoya.kimura <tomoya.kimura@tier4.jp>
2020-02-26 16:15:23 +0900

    fix visualize position of traffic-judge-line

--- src/planning/scenario_planning/scenarios/lane_following/behavior_planning/behavior_velocity_planner/src/scene_module/traffic_light/traffic_light.cpp
+++ src/planning/scenario_planning/scenarios/lane_following/behavior_planning/behavior_velocity_planner/src/scene_module/traffic_light/traffic_light.cpp
@@ -78,7 +78,7 @@ bool TrafficLightModule::run(const autoware_planning_msgs::PathWithLaneId& input
                             judge_point.x() - input.points.at(judge_point_idx - 1).point.pose.position.x);
           tf2::Quaternion quat;
           quat.setRPY(0, 0, yaw);
-          tf2::Transform tf_map2judge_pose(quat, tf2::Vector3(judge_point.x(), judge_point.y(), 0.0));
+          tf2::Transform tf_map2judge_pose(quat, tf2::Vector3(judge_point.x(), judge_point.y(), self_pose.pose.position.z));
           tf2::Transform tf_map2self_pose;
           tf2::Transform tf_judge_pose2self_pose;
           tf2::fromMsg(self_pose.pose, tf_map2self_pose);
