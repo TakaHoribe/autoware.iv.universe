8bc34561efbf4e978df9779ecbecd005d358afcc
Miho Ueno <miho.ueno@tier4.jp>
2020-04-10 11:53:03 +0900

    modify incorrect header

--- src/sensing/driver/microstrain_mips/include/microstrain_mips/microstrain_diagnostic_updater.h
+++ src/sensing/driver/microstrain_mips/include/microstrain_mips/microstrain_diagnostic_updater.h
@@ -1,7 +1,19 @@
-有
-有
-有
-有#include "diagnostic_updater/diagnostic_updater.h"
+/*
+ * Copyright 2020 Tier IV, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "diagnostic_updater/diagnostic_updater.h"
 #include "diagnostic_updater/publisher.h"
 #include "microstrain_3dm.h"
 #include "microstrain_mips/status_msg.h"
