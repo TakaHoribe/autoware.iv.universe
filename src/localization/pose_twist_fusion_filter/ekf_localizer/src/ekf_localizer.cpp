971aa58b3de50d934e1ab1e7d12ed731c2fe82f1
Yamato Ando <yamato.ando@gmail.com>
2020-04-16 21:15:03 +0900

    rename ekf yawbias topic
    Signed-off-by: Yamato Ando <yamato.ando@gmail.com>

--- src/localization/pose_twist_fusion_filter/ekf_localizer/src/ekf_localizer.cpp
+++ src/localization/pose_twist_fusion_filter/ekf_localizer/src/ekf_localizer.cpp
@@ -74,9 +74,9 @@ EKFLocalizer::EKFLocalizer() : nh_(""), pnh_("~"), dim_x_(6 /* x, y, yaw, yaw_bi
   pub_twist_ = nh_.advertise<geometry_msgs::TwistStamped>("ekf_twist", 1);
   pub_twist_cov_ = nh_.advertise<geometry_msgs::TwistWithCovarianceStamped>("ekf_twist_with_covariance", 1);
   pub_yaw_bias_ = pnh_.advertise<std_msgs::Float64>("estimated_yaw_bias", 1);
-  pub_pose_no_yawbias_ = pnh_.advertise<geometry_msgs::PoseStamped>("ekf_pose_without_yawbias", 1);
+  pub_pose_no_yawbias_ = nh_.advertise<geometry_msgs::PoseStamped>("ekf_pose_without_yawbias", 1);
   pub_pose_cov_no_yawbias_ =
-      pnh_.advertise<geometry_msgs::PoseWithCovarianceStamped>("ekf_pose_with_covariance_without_yawbias", 1);
+      nh_.advertise<geometry_msgs::PoseWithCovarianceStamped>("ekf_pose_with_covariance_without_yawbias", 1);
   sub_initialpose_ = nh_.subscribe("initialpose", 1, &EKFLocalizer::callbackInitialPose, this);
   sub_pose_with_cov_ = nh_.subscribe("in_pose_with_covariance", 1, &EKFLocalizer::callbackPoseWithCovariance, this);
   sub_pose_ = nh_.subscribe("in_pose", 1, &EKFLocalizer::callbackPose, this);
