985b05f57eb90a2d0313232a354d07b6035cd7fb
wep21 <border_goldenmarket@yahoo.co.jp>
2020-04-29 00:33:02 +0900

    Avoid finding pcl components(visualization, features, io)
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>

--- localization/pose_estimator/ndt_scan_matcher/ndt_omp/apps/align.cpp
+++ localization/pose_estimator/ndt_scan_matcher/ndt_omp/apps/align.cpp
@@ -31,7 +31,6 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/registration/ndt.h>
-#include <pcl/visualization/pcl_visualizer.h>
 #include <ros/ros.h>
 #include <iostream>
 
@@ -124,19 +123,6 @@ int main(int argc, char ** argv)
       aligned = align(ndt_omp, target_cloud, source_cloud);
     }
   }
-
-  // visulization
-  pcl::visualization::PCLVisualizer vis("vis");
-  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> target_handler(
-    target_cloud, 255.0, 0.0, 0.0);
-  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_handler(
-    source_cloud, 0.0, 255.0, 0.0);
-  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> aligned_handler(
-    aligned, 0.0, 0.0, 255.0);
-  vis.addPointCloud(target_cloud, target_handler, "target");
-  vis.addPointCloud(source_cloud, source_handler, "source");
-  vis.addPointCloud(aligned, aligned_handler, "aligned");
-  vis.spin();
 
   return 0;
 }
