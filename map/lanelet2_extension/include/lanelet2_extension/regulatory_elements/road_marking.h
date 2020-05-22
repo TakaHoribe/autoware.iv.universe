7a384e02f5a7059ea8c08955b72766c3df2f65d6
mitsudome-r <ryohsuke.mitsudome@tier4.jp>
2020-05-22 23:06:28 +0900

    fix license
    Signed-off-by: mitsudome-r <ryohsuke.mitsudome@tier4.jp>

--- map/lanelet2_extension/include/lanelet2_extension/regulatory_elements/road_marking.h
+++ map/lanelet2_extension/include/lanelet2_extension/regulatory_elements/road_marking.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2019 Autoware Foundation. All rights reserved.
+ * Copyright 2020 Tier IV, Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,8 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Authors: Ryohsuke Mitsudome
  */
 
 #ifndef LANELET2_EXTENSION_REGULATORY_ELEMENTS_ROAD_MARKING_H
