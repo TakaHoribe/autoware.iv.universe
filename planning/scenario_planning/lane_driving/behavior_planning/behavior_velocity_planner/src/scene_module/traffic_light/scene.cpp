80939aed99c946945fc72faee81b871142e1760c
Fumiya Watanabe <rej55.g@gmail.com>
2020-06-10 19:34:28 +0900

    Fix/calculate distance in traffic light (#583)
    * Add arclength calculation
    
    * Remove debug messages
    
    * Check lateral distance and add comments
    
    * Change std::hypot to dist
    
    * Check longitudinal distance only

--- planning/scenario_planning/lane_driving/behavior_planning/behavior_velocity_planner/src/scene_module/traffic_light/scene.cpp
+++ planning/scenario_planning/lane_driving/behavior_planning/behavior_velocity_planner/src/scene_module/traffic_light/scene.cpp
@@ -22,12 +22,82 @@
 
 namespace
 {
-double calcDistance(const geometry_msgs::Pose & p1, const Eigen::Vector2d & p2)
+std::pair<int, double> findWayPointAndDistance(
+  const autoware_planning_msgs::PathWithLaneId & input_path, const Eigen::Vector2d & p)
 {
-  const auto dx = p1.position.x - p2.x();
-  const auto dy = p1.position.y - p2.y();
+  constexpr double max_lateral_dist = 3.0;
+  for (size_t i = 0; i < input_path.points.size() - 1; ++i) {
+    const double dx = p.x() - input_path.points.at(i).point.pose.position.x;
+    const double dy = p.y() - input_path.points.at(i).point.pose.position.y;
+    const double dx_wp = input_path.points.at(i + 1).point.pose.position.x -
+                         input_path.points.at(i).point.pose.position.x;
+    const double dy_wp = input_path.points.at(i + 1).point.pose.position.y -
+                         input_path.points.at(i).point.pose.position.y;
 
-  return std::hypot(dx, dy);
+    const double theta = std::atan2(dy, dx) - std::atan2(dy_wp, dx_wp);
+
+    const double dist = std::hypot(dx, dy);
+    const double dist_wp = std::hypot(dx_wp, dy_wp);
+
+    // check lateral distance
+    if(std::fabs(dist*std::sin(theta)) > max_lateral_dist)
+    {
+      continue;
+    }
+
+    // if the point p is back of the way point, return negative distance
+    if(dist*std::cos(theta) < 0) {
+      return std::make_pair(static_cast<int>(i), -1.0 * dist);
+    }
+
+    if (dist*std::cos(theta) < dist_wp) {
+      return std::make_pair(static_cast<int>(i), dist);
+    }
+  }
+
+  // if the way point is not found, return negative distance from the way point at 0
+  const double dx = p.x() - input_path.points.at(0).point.pose.position.x;
+  const double dy = p.y() - input_path.points.at(0).point.pose.position.y;
+  return std::make_pair(-1, -1.0 * std::hypot(dx, dy));
+}
+
+double calcArcLengthFromWayPoint(
+  const autoware_planning_msgs::PathWithLaneId & input_path, const int & src, const int & dst)
+{
+  double length = 0;
+  const size_t src_idx = src >= 0 ? static_cast<size_t>(src) : 0;
+  const size_t dst_idx = dst >= 0 ? static_cast<size_t>(dst) : 0;
+  for (size_t i = src_idx; i < dst_idx; ++i) {
+    const double dx_wp = input_path.points.at(i + 1).point.pose.position.x -
+                         input_path.points.at(i).point.pose.position.x;
+    const double dy_wp = input_path.points.at(i + 1).point.pose.position.y -
+                         input_path.points.at(i).point.pose.position.y;
+    length += std::hypot(dx_wp, dy_wp);
+  }
+  return length;
+}
+
+double calcSignedArcLength(
+  const autoware_planning_msgs::PathWithLaneId & input_path, const geometry_msgs::Pose & p1,
+  const Eigen::Vector2d & p2)
+{
+  std::pair<int, double> src =
+    findWayPointAndDistance(input_path, Eigen::Vector2d(p1.position.x, p1.position.y));
+  std::pair<int, double> dst = findWayPointAndDistance(input_path, p2);
+  if (dst.first == -1) {
+    double dx = p1.position.x - p2.x();
+    double dy = p1.position.y - p2.y();
+    return -1.0 * std::hypot(dx, dy);
+  }
+
+  if (src.first < dst.first) {
+    return calcArcLengthFromWayPoint(input_path, src.first, dst.first) - src.second + dst.second;
+  } else if (src.first > dst.first) {
+    return -1.0 *
+           (calcArcLengthFromWayPoint(input_path, dst.first, src.first) - dst.second + src.second);
+  } else {
+    return dst.second - src.second;
+  }
 }
 
 double calcSignedDistance(const geometry_msgs::Pose & p1, const Eigen::Vector2d & p2)
@@ -76,8 +146,7 @@ bool TrafficLightModule::modifyPathVelocity(autoware_planning_msgs::PathWithLane
   // get vehicle info
   geometry_msgs::TwistStamped::ConstPtr self_twist_ptr = planner_data_->current_velocity;
   const double stop_border_distance_threshold = getStopBorderDistanceThreshold(
-    self_twist_ptr->twist.linear.x,
-    planner_param_.max_stop_acceleration_threshold,
+    self_twist_ptr->twist.linear.x, planner_param_.max_stop_acceleration_threshold,
     planner_param_.delay_response_time);
 
   geometry_msgs::PoseStamped self_pose = planner_data_->current_pose;
@@ -99,7 +168,8 @@ bool TrafficLightModule::modifyPathVelocity(autoware_planning_msgs::PathWithLane
           continue;
         }
 
-        if (isOverJudgePoint(self_pose.pose, input_path, judge_point_idx, judge_point, judge_range)) {
+        if (isOverJudgePoint(
+              self_pose.pose, input_path, judge_point_idx, judge_point, judge_range)) {
           state_ = State::GO_OUT;
           return true;
         }
@@ -115,7 +185,9 @@ bool TrafficLightModule::modifyPathVelocity(autoware_planning_msgs::PathWithLane
           continue;
         }
 
-        if (calcSignedDistance(self_pose.pose, stop_line_point) < stop_border_distance_threshold) {
+        if (
+          calcSignedArcLength(input_path, self_pose.pose, stop_line_point) <
+          stop_border_distance_threshold) {
           ROS_WARN_THROTTLE(1.0, "[traffic_light] vehicle is over stop border");
           return true;
         }
@@ -143,7 +215,7 @@ bool TrafficLightModule::isOverJudgePoint(
   const geometry_msgs::Pose & self_pose, const autoware_planning_msgs::PathWithLaneId & input_path,
   const size_t & judge_point_idx, const Eigen::Vector2d & judge_point, const double judge_range)
 {
-  if (calcDistance(self_pose, judge_point) > judge_range) {
+  if (calcSignedArcLength(input_path, self_pose, judge_point) > judge_range) {
     return false;
   }
 
